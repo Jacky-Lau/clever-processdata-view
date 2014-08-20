e6953a73338d10ab6e51cf5267b8d33c7a2e15aa
caihailing@hotmail.com <caihailing@hotmail.com>
2014-08-20 09:48:45 +0800

    修改日志报错

--- src/main/java/org/zju/clever/processdata/view/controller/MainController.java
+++ src/main/java/org/zju/clever/processdata/view/controller/MainController.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
-
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,75 +16,59 @@ import org.zju.clever.processdata.view.service.MainService;
 
 @Controller
 public class MainController {
-	public final static Logger logger = Logger.getLogger(MainController.class);
-
 	@Resource(name = "mainService")
 	private MainService mainService;
 
 	@RequestMapping(value = "/processData/{patientId}/{dataType}/{dataIndex}")
 	public ModelAndView getView(@PathVariable String patientId,
-			@PathVariable String dataType, @PathVariable String dataIndex) {
+			@PathVariable String dataType, @PathVariable String dataIndex) throws Exception{
 		Map<String, Object> model = new HashMap<String, Object>();
-		try {
-			logger.debug("user mainService");
-			String type = "";
-			String indexName = "";
-			String finalState = "";
-			List<Action> actions = new ArrayList<Action>();
-			if (dataType.equals("emr")) {
-				logger.debug("emr");
-				type = "EMR";
-				indexName = "病历文档号";
-				actions = this.mainService.getEMRActions(dataIndex);
+		String type = "";
+		String indexName = "";
+		String finalState = "";
+		List<Action> actions = new ArrayList<Action>();
+		if (dataType.equals("emr")) {
+			type = "EMR";
+			indexName = "病历文档号";
+			actions = this.mainService.getEMRActions(dataIndex);
 
-			} else if (dataType.equals("firstpage")) {
-				logger.debug("firstpage");
-				type = "病案首页";
-				indexName = "病案首页号";
-				actions = this.mainService.getEMRFirstPageActions(dataIndex);
-			} else if (dataType.equals("exam")) {
-				logger.debug("exam");
-				type = "检查";
-				indexName = "检查申请单号";
-				actions = this.mainService.getExamActions(dataIndex);
-			} else if (dataType.equals("labTest")) {
-				logger.debug("labTest");
-				type = "检验";
-				indexName = "检验申请单号";
-				actions = this.mainService.getLabTestActions(dataIndex);
-			} else if (dataType.equals("operation")) {
-				logger.debug("operation");
-				type = "手术";
-				indexName = "手术申请号";
-				actions = this.mainService.getOperationActions(dataIndex);
-			} else if (dataType.equals("order")) {
-				logger.debug("order");
-				type = "医嘱";
-				indexName = "医嘱号";
-				actions = this.mainService.getOrderActions(dataIndex);
-			} else if (dataType.equals("presc")) {
-				logger.debug("presc");
-				type = "处方";
-				indexName = "处方号";
-				actions = this.mainService.getPrescActions(dataIndex);
-			} else {
-				throw new Exception("Data type " + dataType
-						+ " is not supported.");
-			}
-			logger.debug("dataType:" + dataType);
-			finalState = actions.get(actions.size() - 1).getActionType();
-
-			PatientIndex patientIndex = this.mainService
-					.getPatientIndexById(patientId);
-			model.put("patientIndex", patientIndex);
-			model.put("actions", actions);
-			model.put("type", type);
-			model.put("dataIndex", dataIndex);
-			model.put("indexName", indexName);
-			model.put("finalState", finalState);
-		} catch (Exception ex) {
-			logger.error("MainController Error:"+ex.getStackTrace().toString());
+		} else if (dataType.equals("firstpage")) {
+			type = "病案首页";
+			indexName = "病案首页号";
+			actions = this.mainService.getEMRFirstPageActions(dataIndex);
+		} else if (dataType.equals("exam")) {
+			type = "检查";
+			indexName = "检查申请单号";
+			actions = this.mainService.getExamActions(dataIndex);
+		} else if (dataType.equals("labTest")) {
+			type = "检验";
+			indexName = "检验申请单号";
+			actions = this.mainService.getLabTestActions(dataIndex);
+		} else if (dataType.equals("operation")) {
+			type = "手术";
+			indexName = "手术申请号";
+			actions = this.mainService.getOperationActions(dataIndex);
+		} else if (dataType.equals("order")) {
+			type = "医嘱";
+			indexName = "医嘱号";
+			actions = this.mainService.getOrderActions(dataIndex);
+		} else if (dataType.equals("presc")) {
+			type = "处方";
+			indexName = "处方号";
+			actions = this.mainService.getPrescActions(dataIndex);
+		} else {
+			throw new Exception("Data type " + dataType + " is not supported.");
 		}
+		finalState = actions.get(actions.size() - 1).getActionType();
+
+		PatientIndex patientIndex = this.mainService
+				.getPatientIndexById(patientId);
+		model.put("patientIndex", patientIndex);
+		model.put("actions", actions);
+		model.put("type", type);
+		model.put("dataIndex", dataIndex);
+		model.put("indexName", indexName);
+		model.put("finalState", finalState);
 		return new ModelAndView("index", model);
 	}
 }
